f68d927d777ea93a87944c6904d35cb21929dd34
Abhishek <abhirai2006a@gmail.com>
2025-07-17 22:10:42 +0530

    Add files via upload

---
+++ L 10-/app.js
@@ -0,0 +1,53 @@
+const express =require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+app.get("/",(req,res,next) =>{
+    console.log("handling  / for get ",req.url,req.method);
+    next();
+});
+
+
+app.get("/contact-us",(req,res,next) =>{
+    console.log("Handling contact us",req.url,req.method);
+    res.send(`
+        <h1> Please Give your details <h1>
+        <form action ="/contact-us" method ="POST">
+
+        <input type="text" name ="name" placeholder ="Enter your name"/>
+        <input type="email" name ="email" placeholder ="Enter your Email"/>
+
+        <input type="submit"/>
+
+        </form>
+        `)
+    next();
+});
+
+app.post("/contact-us",(req,res,next) =>{
+    console.log("first Handling",req.url,req.body);
+    next();
+});
+
+app.use(bodyParser.urlencoded());
+
+app.post("/contact-us",(req,res,next) =>{
+    console.log("Post Handling",req.url,req.body);
+    res.send(`<h1> We will contact you shortly</h1>`)
+    next();
+});
+
+// app.use((req,res,next) =>{
+//     console.log("Third dummy middleware",req.url,req.method);
+//     res.send("<h1>Welcome to Abhishek's repo..</h1>")
+//     next();
+// });
+
+
+
+
+
+const PORT =3001;
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
